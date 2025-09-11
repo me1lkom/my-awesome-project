dd9f7bd6dc8637bc692b0c77b7f748101b0bb90d
me1lkom <melkom06@gmail.com>
2025-09-11 14:50:39 +0300

    Добавили валидацию email

---
+++ src/script.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('contactForm'); // получаем элемент формы
+const emailInput = document.getElementById('email'); // получаем поле ввода email
+
+emailInput.addEventListener('input', validateEmail); // на событие ввода email будет вызываться функция validateEmail()
+
+function validateEmail() { // функция проверки корректности email
+    const emailRegex = /[A-Za-z0-9_-\.]{2,15}@[a-z0-9\.-_]{1,10}\.{a-z}{2,3}/;
+    if(emailRegex.test(emailInput.value)){
+        removeError(emailInput);  
+        return true;
+    }
+    else{
+        showError(emailInput, "Email не соответствует формату. Пример: ivanov@mail.ru")
+        return false;
+    }
+}
+
+function showError(input, massage) { // функция покаха ошибки
+    const formControl = input.parentElement; // получаем родительскую форму
+    const errorElement = formControl.querrySelector('.error') || document.createElement('div'); //ищем существующий блок ошибки, если нет создаём
+
+    errorElement.class = 'error'; // css стиль для блока ошибки
+    errorElement.textContent = massage; // задаём сообщение об ошибки из параметра функции
+
+    formControl.appendChild(errorElement); //добавляем компонент на форму
+    input.style.borderColor = 'red';
+}
+
+function removeError(input) { // функция скрытия ошибки
+    const formControl = input.parentElement; // получаем родительскую форму
+    const errorElement = formControl.querrySelector('.error') // ищем существующий блок ошщибки
+
+    if(errorElement){ // если ошибка найдена
+        formControl.removeChild(errorElement); // удаляем блок с ошибкой
+    }
+
+    input.style.borderColor = 'green'; // задаем зеленый цвет обводки
+}
